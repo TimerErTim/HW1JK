570394e2a7ce126725b5c27387eb72bb42202a80
leberkaspepi <jonas.kellermayr@gmail.com>
2020-10-04 23:00:00 +0200

    backend complete

--- Project/src/com/HW1/primeSieve/EratosthenesPrimeSieve.java
+++ Project/src/com/HW1/primeSieve/EratosthenesPrimeSieve.java
@@ -1,6 +1,7 @@
 package com.HW1.primeSieve;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class EratosthenesPrimeSieve implements PrimeSieve {
     private int limit;
@@ -33,5 +34,39 @@ public class EratosthenesPrimeSieve implements PrimeSieve {
         for (Integer i : primesInRange)
             System.out.println(i);
     }
-}
 
+    public void printResult() {
+        // collection of numbers and their addends
+        ArrayList<Integer[]> results = new ArrayList<>();
+
+        // filling the list
+        for (int num = 4; num <= limit; num += 2) {
+            for (Integer i : primesInRange) {
+                if (i >= (num - 1))
+                    break;
+
+                int dif = num - i;
+
+                if (isPrime(dif))
+                    //System.out.println(String.format("%s Summe: %s + %s", num, i, dif));
+                    results.add(new Integer[]{num, i, dif});
+            }
+        }
+
+        // filtering out duplicates
+        for (int i = 0; i < results.size() - 1; i++) {
+            for (int j = i + 1; j < results.size(); j++) {
+                Integer[] arr1 = results.get(i);
+                Integer[] arr2 = results.get(j);
+
+                if (Objects.equals(arr1[0], arr2[0])) {
+                    if (Objects.equals(arr1[1], arr2[2]))
+                        results.remove(j);
+                }
+            }
+        }
+
+        for (Integer[] arr : results)
+            System.out.printf("%s Summe: %s + %s%n", arr[0], arr[1], arr[2]);
+    }
+}
