5ff4f4f7f78baf46f72a7bb465ffa5c7ecc6b51c
leberkaspepi <jonas.kellermayr@gmail.com>
2020-10-04 23:01:49 +0200

    project launchable

---
+++ Project/src/com/HW1/main/Main.java
@@ -0,0 +1,10 @@
+package com.HW1.main;
+
+import com.HW1.primeSieve.EratosthenesPrimeSieve;
+
+public class Main {
+    public static void main(String[] args) {
+        EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve(10);
+        eps.printResult();
+    }
+}
